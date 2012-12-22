48d0dedb9478961c0ca6ef3d91cfb441b5edab4c
Satyajit Sarangi <satyajit.sarangi@gmail.com>
2012-12-22 02:04:21 -0800

    Fixed NVariableDecl bug where AssignmentExpr was not being set to NULL

--- MiniC_llvm/src/main.cpp
+++ MiniC_llvm/src/main.cpp
@@ -5,10 +5,20 @@
 using namespace std;
 
 extern int yyparse();
+extern FILE *yyin;
 extern NBlock* programBlock;
 
 int main(int argc, char **argv)
 {
+    FILE *inpFile = fopen(argv[1], "r");
+    if (!inpFile)
+    {
+        cout << "Error opening File" << endl;
+        return -1;
+    }
+
+    yyin = inpFile;
+
 	yyparse();
 	std::cout << programBlock << endl;
     // see http://comments.gmane.org/gmane.comp.compilers.llvm.devel/33877
